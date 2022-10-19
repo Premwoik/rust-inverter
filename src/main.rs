da83605e6ad1ae8ed4aab75d1de1e464c7fcb24e
Przemysław Wojtasik <przemyslaw.wojtasik@erlang-solutions.com>
2022-10-19 20:51:40 +0200

    Print received interrupts

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
-mod inverter_api;
-
 use std::error::Error;
 use std::thread;
 use std::time::Duration;
 
 use rppal::gpio::Gpio;
-use rppal::gpio::Level;
 use rppal::gpio::Trigger;
 
 const GPIO_COUNTER_OUTPUT: u8 = 6;
@@ -13,19 +10,15 @@ const GPIO_COUNTER_INPUT: u8 = 5;
 
 fn main() -> Result<(), Box<dyn Error>> {
     let gpio = Gpio::new()?;
+    
+    let mut input = gpio.get(GPIO_COUNTER_INPUT)?.into_input_pullup();
+    input.set_async_interrupt(Trigger::RisingEdge, |_| {println!("input");})?;
 
-    gpio.get(GPIO_COUNTER_OUTPUT)?
-        .into_input_pullup()
-        .set_async_interrupt(Trigger::RisingEdge, |_: Level| -> () {
-            println!("Read output")
-        })?;
-    gpio.get(GPIO_COUNTER_INPUT)?
-        .into_input_pullup()
-        .set_async_interrupt(Trigger::RisingEdge, |_: Level| -> () {
-            println!("Read input")
-        })?;
+    let mut output = gpio.get(GPIO_COUNTER_OUTPUT)?.into_input_pullup();
+    output.set_async_interrupt(Trigger::RisingEdge, |_| println!("output"))?;
 
     loop {
-        thread::sleep(Duration::from_secs(5))
+        thread::sleep(Duration::from_secs(1));
+        println!("loop");
     }
 }
